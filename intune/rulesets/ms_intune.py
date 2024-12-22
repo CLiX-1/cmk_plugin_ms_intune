701ca5926a188f8d5c710115425132dc9213a7a7
C. Pommer <39278813+CLiX-1@users.noreply.github.com>
2024-12-22 17:51:57 +0100

    Added proxy, timeout options and general improvememts

--- intune/rulesets/ms_intune.py
+++ intune/rulesets/ms_intune.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# -*- coding: utf-8; py-indent-offset: 4; max-line-length: 100 -*-
 
 # Copyright (C) 2024  Christopher Pommer <cp.software@outlook.de>
 
@@ -18,18 +18,26 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
+####################################################################################################
+# Checkmk ruleset to configure the Microsoft Intune special agent.
+
+
 from cmk.rulesets.v1 import Help, Message, Title
 from cmk.rulesets.v1.form_specs import (
     DefaultValue,
     DictElement,
     Dictionary,
     FieldSize,
+    InputHint,
     MultipleChoice,
     MultipleChoiceElement,
     Password,
+    Proxy,
     String,
+    TimeMagnitude,
+    TimeSpan,
 )
-from cmk.rulesets.v1.form_specs.validators import LengthInRange, MatchRegex
+from cmk.rulesets.v1.form_specs.validators import LengthInRange, MatchRegex, NumberInRange
 from cmk.rulesets.v1.rule_specs import SpecialAgent, Topic
 
 
@@ -52,12 +60,17 @@ def _parameter_form_special_agent_ms_intune() -> Dictionary:
                     field_size=FieldSize.LARGE,
                     custom_validate=[
                         MatchRegex(
-                            regex="^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
-                            error_msg=Message("Tenant ID / Directory ID must be in 36-character GUID format."),
+                            regex="^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-"
+                            "[0-9a-fA-F]{12}$",
+                            error_msg=Message(
+                                "Tenant ID / Directory ID must be in 36-character GUID format."
+                            ),
                         ),
                         LengthInRange(
                             min_value=36,
-                            error_msg=Message("Tenant ID / Directory ID must be in 36-character GUID format."),
+                            error_msg=Message(
+                                "Tenant ID / Directory ID must be in 36-character GUID format."
+                            ),
                         ),
                     ],
                 ),
@@ -66,16 +79,24 @@ def _parameter_form_special_agent_ms_intune() -> Dictionary:
             "app_id": DictElement(
                 parameter_form=String(
                     title=Title("Client ID / Application ID"),
-                    help_text=Help("The ID of the Micrsoft Entra app registration for Microsoft Graph API requests."),
+                    help_text=Help(
+                        "The App ID of the Micrsoft Entra app registration for Microsoft Graph API "
+                        "requests."
+                    ),
                     field_size=FieldSize.LARGE,
                     custom_validate=[
                         MatchRegex(
-                            regex="^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
-                            error_msg=Message("Client ID / Application ID must be in 36-character GUID format."),
+                            regex="^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-"
+                            "[0-9a-fA-F]{12}$",
+                            error_msg=Message(
+                                "Client ID / Application ID must be in 36-character GUID format."
+                            ),
                         ),
                         LengthInRange(
                             min_value=36,
-                            error_msg=Message("Client ID / Application ID must be in 36-character GUID format."),
+                            error_msg=Message(
+                                "Client ID / Application ID must be in 36-character GUID format."
+                            ),
                         ),
                     ],
                 ),
@@ -88,20 +109,30 @@ def _parameter_form_special_agent_ms_intune() -> Dictionary:
                 ),
                 required=True,
             ),
+            "proxy": DictElement(
+                parameter_form=Proxy(
+                    title=Title("HTTP proxy"),
+                    help_text=Help(
+                        "The HTTP proxy used to connect to the Microsoft Graph API. If not set, "
+                        "the environment settings will be used."
+                    ),
+                ),
+            ),
             "services_to_monitor": DictElement(
                 parameter_form=MultipleChoice(
                     title=Title("Microsoft Intune services to monitor"),
                     help_text=Help(
                         "Select the Microsoft Intune services that you want to monitor. Ensure "
-                        "that you add the required Microsoft Graph API permissions to "
-                        "your Microsoft Entra app registration and grant admin consent "
-                        "to them. For App licenses, you must configure at least the "
+                        "that you add the required Microsoft Graph API permissions to your "
+                        "Microsoft Entra app registration and grant admin consent to them. For"
+                        "App licenses, you must configure at least the "
                         "<tt>DeviceManagementApps.Read.All</tt> API application permission. "
-                        "For Apple ADE tokens, Apple MDM push certificate "
-                        "and VPP tokens, you must configure at least the "
-                        "<tt>DeviceManagementServiceConfig.Read.All</tt> API application permission. "
-                        "For Certificate connectors, you must configure at least the "
-                        "<tt>DeviceManagementConfiguration.Read.All</tt> API application permission. "
+                        "For Apple ADE tokens, Apple MDM push certificate and VPP tokens, you"
+                        "must configure at least the "
+                        "<tt>DeviceManagementServiceConfig.Read.All</tt> API application "
+                        "permission. For Certificate connectors, you must configure at least the "
+                        "<tt>DeviceManagementConfiguration.Read.All</tt> API application "
+                        "permission. "
                     ),
                     elements=[
                         MultipleChoiceElement(
@@ -117,7 +148,8 @@ def _parameter_form_special_agent_ms_intune() -> Dictionary:
                             title=Title("Apple MDM push certificate"),
                         ),
                         MultipleChoiceElement(
-                            name="vpp_tokens", title=Title("Apple VPP tokens (Volume Purchase Program)")
+                            name="vpp_tokens",
+                            title=Title("Apple VPP tokens (Volume Purchase Program)"),
                         ),
                         MultipleChoiceElement(
                             name="cert_connectors",
@@ -127,7 +159,9 @@ def _parameter_form_special_agent_ms_intune() -> Dictionary:
                     custom_validate=[
                         LengthInRange(
                             min_value=1,
-                            error_msg=Message("Select one or more <b>Microsoft Intune services to monitor</b>"),
+                            error_msg=Message(
+                                "Select one or more <b>Microsoft Intune services to monitor</b>"
+                            ),
                         ),
                     ],
                     prefill=DefaultValue(
@@ -141,6 +175,25 @@ def _parameter_form_special_agent_ms_intune() -> Dictionary:
                     ),
                 ),
                 required=True,
+            ),
+            "timeout": DictElement(
+                parameter_form=TimeSpan(
+                    title=Title("Timeout for each API request"),
+                    help_text=Help(
+                        "Define a custom timeout in seconds to use for each API request. The "
+                        "timeout is used for token request and any service that should be "
+                        "monitored. The default timeout is 10s."
+                    ),
+                    displayed_magnitudes=[TimeMagnitude.SECOND],
+                    prefill=InputHint(10.0),
+                    custom_validate=[
+                        NumberInRange(
+                            min_value=3,
+                            max_value=600,
+                            error_msg=Message("The timeout must be between 3s and 600s."),
+                        ),
+                    ],
+                ),
             ),
         },
     )
--- intune/server_side_calls/ms_intune.py
+++ intune/server_side_calls/ms_intune.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# -*- coding: utf-8; py-indent-offset: 4; max-line-length: 100 -*-
 
 # Copyright (C) 2024  Christopher Pommer <cp.software@outlook.de>
 
@@ -18,14 +18,22 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
-from collections.abc import Iterator, Sequence
+####################################################################################################
+# This is part of the Checkmk special agent for monitoring Microsoft Intune services.
+# It builds the configuration parameters for the special agent call.
+
+
 from pydantic import BaseModel
+from typing import Optional, Iterator, List
 
 from cmk.server_side_calls.v1 import (
+    EnvProxy,
     HostConfig,
+    NoProxy,
     Secret,
     SpecialAgentCommand,
     SpecialAgentConfig,
+    URLProxy,
 )
 
 
@@ -33,30 +41,42 @@ class Params(BaseModel):
     tenant_id: str
     app_id: str
     app_secret: Secret
-    services_to_monitor: Sequence[str] = []
+    proxy: Optional[URLProxy | NoProxy | EnvProxy] = None
+    services_to_monitor: List[str]
+    timeout: Optional[float] = 10.0
 
 
-def commands_function(
+def generate_special_agent_commands(
     params: Params,
     _host_config: HostConfig,
 ) -> Iterator[SpecialAgentCommand]:
-    args: Sequence[str | Secret] = [
+    args: List[str | Secret] = [
         "--tenant-id",
         params.tenant_id,
         "--app-id",
         params.app_id,
         "--app-secret",
         params.app_secret,
+        "--services-to-monitor",
+        ",".join(params.services_to_monitor),
+        "--timeout",
+        str(params.timeout),
     ]
 
-    if params.services_to_monitor:
-        args += ["--services-to-monitor", ",".join(params.services_to_monitor)]
+    if params.proxy:
+        match params.proxy:
+            case URLProxy(url):
+                args += ["--proxy", url]
+            case EnvProxy():
+                args += ["--proxy", "FROM_ENVIRONMENT"]
+            case NoProxy():
+                args += ["--proxy", "NO_PROXY"]
 
     yield SpecialAgentCommand(command_arguments=args)
 
 
-special_agent_ms_intune = SpecialAgentConfig(
+special_agent_ms_entra = SpecialAgentConfig(
     name="ms_intune",
     parameter_parser=Params.model_validate,
-    commands_function=commands_function,
+    commands_function=generate_special_agent_commands,
 )
